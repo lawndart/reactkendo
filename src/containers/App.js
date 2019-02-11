ffa2773ab3cdd41790ebcbc938414e521dba9c5c
Brian Lisowski <brianlisowski@Brians-MacBook-Pro.local>
2019-02-11 10:47:49 -0500

    got inline editing working

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import { NumericTextBox } from '@progress/kendo-react-inputs';
 import { Button } from '@progress/kendo-react-buttons';
 import { filterBy } from '@progress/kendo-data-query';
 import { Grid , GridColumn as Column } from '@progress/kendo-react-grid';
+import MyCommandCell from '../components/command-cell.js';
 import '@progress/kendo-theme-default/dist/all.css';
 import '../assets/css/App.css';
 
@@ -11,6 +12,9 @@ import '../assets/css/App.css';
 import product from '../assets/json/products.json';
 
 class App extends Component {
+
+  CommandCell;
+  
   constructor(props) {
     super(props)
     
@@ -37,6 +41,15 @@ class App extends Component {
         "Do stuff"
       ]
     };
+
+    this.enterInsert = this.enterInsert.bind(this);
+    this.itemChange = this.itemChange.bind(this);
+
+    const enterEdit = this.enterEdit.bind(this);
+    const save = this.save.bind(this);
+    const cancel = this.cancel.bind(this);
+    const remove = this.remove.bind(this);
+    this.CommandCell = MyCommandCell(enterEdit,remove,save,cancel, "inEdit");
   }
 
   handleNameChange = (event) => {
@@ -67,6 +80,87 @@ class App extends Component {
   }
 
   getProducts = (filter) => filterBy(product, filter);
+
+  
+
+  enterEdit(dataItem) {
+    this.update(this.state.data, dataItem).inEdit = true;
+    this.setState({
+        data: this.state.data.slice()
+    });
+  }
+
+  enterInsert() {
+    const dataItem = { inEdit: true, Discontinued: false };
+    const newproducts = this.state.data.slice();
+    newproducts.unshift(dataItem);
+    this.update(newproducts, dataItem);
+    this.setState({
+        data: newproducts
+    });
+  }
+
+  save(dataItem) {
+    dataItem.inEdit = undefined;
+    dataItem.ProductID = this.update(product, dataItem).ProductID;
+    this.setState({
+      data: this.state.data.slice()
+    });
+  }
+
+  cancel(dataItem) {
+    if (dataItem.ProductID) {
+        let originalItem = product.find(p => p.ProductID === dataItem.ProductID);
+        this.update(this.state.data, originalItem);
+    } else {
+        this.update(this.state.data, dataItem, !dataItem.ProductID);
+    }
+    this.setState({
+        data: this.state.data.slice()
+    });
+  }
+
+  remove(dataItem) {
+    dataItem.inEdit = undefined;
+    this.update(this.state.data, dataItem, true);
+    this.update(product, dataItem, true);
+    this.setState({
+        data: this.state.data.slice()
+    });
+  }
+
+  update(data, item, remove) {
+     let updated;
+     let index = data.findIndex(p => p === item || item.ProductID && p.ProductID === item.ProductID);
+     if (index >= 0) {
+         updated = Object.assign({}, item);
+         data[index] = updated;
+     } else {
+         let id = 1;
+         data.forEach(p => { id = Math.max(p.ProductID + 1, id); });
+         updated = Object.assign({}, item, { ProductID: id });
+         data.unshift(updated);
+         index = 0;
+      }
+     if (remove) {
+         data = data.splice(index, 1);
+     }
+     return data[index];
+   }
+
+  itemChange(event) {
+    const value = event.value;
+    const name = event.field;
+    if (!name) {
+        return;
+    }
+    const updatedData = this.state.data.slice();
+    const item = this.update(updatedData, event.dataItem);
+    item[name] = value;
+    this.setState({
+        data: updatedData
+    });
+  }
 
   render() {
     return (
@@ -107,11 +201,15 @@ class App extends Component {
             style={{maxHeight: '500px'}}
             filterable={true}
             filter={this.state.filter}
-            onFilterChange={this.handleFilterChange}>
+            onFilterChange={this.handleFilterChange}
+            editField="inEdit"
+            onItemChange={this.itemChange}
+            >
             <Column field="ProductID" title="Product ID" filter="numeric"/>
             <Column field="ProductName" title="Product Name" />
             <Column field="UnitsInStock" title="Number in stock" filter="numeric"/>
             <Column field="UnitsOnOrder" title="Number on order" filter="numeric"/>
+            <Column cell={this.CommandCell} width="180px" filterable={false}/>
           </Grid>
         </section>
       </div>  
